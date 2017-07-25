eb8a426a2c7c0420b287920b442ce15e2db5e641
llj2 <llj2@meitu.com>
2017-07-25 23:11:48 +0800

    这样index，create两个方法就都可用了

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,9 +2,19 @@
 
 class Home extends Controller
 {
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = $this->model('User');
+    }
+
     public function index($name = '')
     {
-//       echo $user->name; //打印出 User model 里面的值（通过 model 传数据）
+        // echo $name;  // 通过 controller 传数据
+        $user = $this->user;
+        $user->name = $name;
+//        echo $user->name; //打印出 User model 里面的值（通过 model 传数据）
         $this->view('home/index', ['name' => $user->name]); // User model 里面的值传到view（通过 view 传数据）
     }
 
