7ce24a4ec615563edc8726559c799f62b3d1c79b
llj2 <llj2@meitu.com>
2017-07-25 23:00:51 +0800

    临时提交2：almost done，有一个改进的写法

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,10 +2,17 @@
 
 class Home extends Controller
 {
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = $this->model('User');
+    }
+
     public function index($name = '')
     {
         // echo $name;  // 通过 controller 传数据
-        $user = $this->model('User'); // $user is the User model
+        $user = $this->user;
         $user->name = $name;
 //        echo $user->name; //打印出 User model 里面的值（通过 model 传数据）
         $this->view('home/index', ['name' => $user->name]); // User model 里面的值传到view（通过 view 传数据）
@@ -13,6 +20,9 @@ class Home extends Controller
 
     public function create($username = '', $email = '')
     {
-
+        $this->user->create([
+            'username' => $username,
+            'email' => $email
+        ]);
     }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -4,4 +4,8 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 class User extends Eloquent
 {
     public $name;
+
+//    public $timestamps = [];
+
+    protected $fillable = ['username', 'email'];
 }
