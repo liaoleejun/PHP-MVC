89f3fa4c5334bf609b331d6d4131046156a4d5db
llj2 <llj2@meitu.com>
2017-07-25 23:09:57 +0800

    临时提交：http://localhost/mvcliao/public/home/create/foo/foo@bar.com 是好的，但是 http://localhost/mvcliao/public/home/index/alice 不可用

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,25 +2,15 @@
 
 class Home extends Controller
 {
-    protected $user;
-
-    public function __construct()
-    {
-        $this->user = $this->model('User');
-    }
-
     public function index($name = '')
     {
-        // echo $name;  // 通过 controller 传数据
-        $user = $this->user;
-        $user->name = $name;
-//        echo $user->name; //打印出 User model 里面的值（通过 model 传数据）
+//       echo $user->name; //打印出 User model 里面的值（通过 model 传数据）
         $this->view('home/index', ['name' => $user->name]); // User model 里面的值传到view（通过 view 传数据）
     }
 
     public function create($username = '', $email = '')
     {
-        $this->user->create([
+        User::create([
             'username' => $username,
             'email' => $email
         ]);
